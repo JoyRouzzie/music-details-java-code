b9c7ac0f550b1ac4597c57b7e97d3c21d0d9cd73
JoyRouzzie <57030204+JoyRouzzie@users.noreply.github.com>
2021-05-10 11:34:26 +0300

    Create musicdetails.java

---
+++ musicdetails.java
@@ -0,0 +1,50 @@
+
+
+/*This a song by Meddy called Vazi, it was released in the year 2020.
+  This a good pop song and stands as at 3.5m million views as per now.
+ 
+ */
+
+ 
+public class musicdetails {
+    public static void main (String [] args){
+        
+        String g = "genre";
+        String m = "name";
+        String fd = "4:07";
+        
+        int n;
+        
+        
+        
+        
+        
+        
+       
+       
+        g = "pop";
+        m = "Meddy";
+        n = 3500000;
+        
+              
+    
+    System.out.println("The song genre is " +g);
+    System.out.println("The musician is " +m);
+    System.out.println("The number of views is " +n);
+    System.out.println("The song's duration is " +fd);
+    
+    }
+        
+}
+               
+                
+        
+           
+    
+    
+
+    
+    
+    
+    
+
